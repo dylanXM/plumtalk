24419124a259cbf52f6d8750333a086efcf1173f
dylan_xm <dylan_xm@163.com>
2024-02-03 15:10:47 +0800

    feat: lint

--- service/src/modules/chatgpt/openai.ts
+++ service/src/modules/chatgpt/openai.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { get_encoding } from '@dqbd/tiktoken'
+import { get_encoding } from '@dqbd/tiktoken';
 import { removeSpecialCharacters } from '@/common/utils';
 
-const tokenizer = get_encoding('cl100k_base')
+const tokenizer = get_encoding('cl100k_base');
 
 interface SendMessageResult {
   id?: string;
@@ -11,16 +11,15 @@ interface SendMessageResult {
   detail?: any;
 }
 
-function getFullUrl(proxyUrl){
+function getFullUrl(proxyUrl) {
   const processedUrl = proxyUrl.endsWith('/') ? proxyUrl.slice(0, -1) : proxyUrl;
-  const baseUrl = processedUrl || 'https://api.openai.com'
-  return `${baseUrl}/v1/chat/completions`
+  const baseUrl = processedUrl || 'https://api.openai.com';
+  return `${baseUrl}/v1/chat/completions`;
 }
 
-export  function sendMessageFromOpenAi(messagesHistory,  inputs ){
-  const { onProgress, maxToken, apiKey, model, temperature = 0.95, proxyUrl } = inputs
-  console.log('current request options: ',apiKey, model, maxToken, proxyUrl );
-  const max_tokens = compilerToken(model,  maxToken)
+export function sendMessageFromOpenAi(messagesHistory, inputs) {
+  const { onProgress, maxToken, apiKey, model, temperature = 0.95, proxyUrl } = inputs;
+  const max_tokens = compilerToken(model, maxToken);
   const options: AxiosRequestConfig = {
     method: 'POST',
     url: getFullUrl(proxyUrl),
@@ -34,17 +33,21 @@ export  function sendMessageFromOpenAi(messagesHistory,  inputs ){
       stream: true,
       temperature,
       model,
-      messages: messagesHistory
+      messages: messagesHistory,
     },
   };
-  const prompt = messagesHistory[messagesHistory.length-1]?.content
-  return new Promise(async (resolve, reject) =>{
+  console.log('current request options: ', options, JSON.stringify(messagesHistory));
+  const prompt = messagesHistory[messagesHistory.length - 1]?.content;
+  return new Promise(async (resolve, reject) => {
     try {
       const response: any = await axios(options);
       const stream = response.data;
-      let result: any = { text: '' };
+      const result: any = { text: '' };
       stream.on('data', (chunk) => {
-        const splitArr = chunk.toString().split('\n\n').filter((line) => line.trim() !== '');
+        const splitArr = chunk
+          .toString()
+          .split('\n\n')
+          .filter((line) => line.trim() !== '');
         for (const line of splitArr) {
           const data = line.replace('data:', '');
           let ISEND = false;
@@ -72,63 +75,61 @@ export  function sendMessageFromOpenAi(messagesHistory,  inputs ){
               }
               result.detail = parsedData;
             }
-            onProgress && onProgress({text:result.text})
+            onProgress && onProgress({ text: result.text });
           } catch (error) {
-            console.log('parse Error', data )
+            console.log('parse Error', data);
           }
         }
       });
-  
+
       stream.on('end', () => {
         // 手动计算token
-        if(result.detail && result.text){
-          const promptTokens = getTokenCount(prompt)
-          const completionTokens = getTokenCount(result.text)
+        if (result.detail && result.text) {
+          const promptTokens = getTokenCount(prompt);
+          const completionTokens = getTokenCount(result.text);
           result.detail.usage = {
             prompt_tokens: promptTokens,
-            completion_tokens: completionTokens ,
+            completion_tokens: completionTokens,
             total_tokens: promptTokens + completionTokens,
-            estimated: true
-          }
+            estimated: true,
+          };
         }
         return resolve(result);
-      }); 
+      });
     } catch (error) {
-      reject(error)
+      reject(error);
     }
-  })
+  });
 }
-
-
 
 export function getTokenCount(text: string) {
-  if(!text) return 0;
-  text = text.replace(/<\|endoftext\|>/g, '')
-  return tokenizer.encode(text).length
+  if (!text) return 0;
+  text = text.replace(/<\|endoftext\|>/g, '');
+  return tokenizer.encode(text).length;
 }
 
-function compilerToken(model, maxToken){
-  let max = 0
+function compilerToken(model, maxToken) {
+  let max = 0;
 
   /* 3.5 */
-  if(model.includes(3.5)){
-    max = maxToken > 4096 ? 4096 : maxToken
+  if (model.includes(3.5)) {
+    max = maxToken > 4096 ? 4096 : maxToken;
   }
 
   /* 4.0 */
-  if(model.includes('gpt-4')){
-    max = maxToken > 8192 ? 8192 : maxToken
+  if (model.includes('gpt-4')) {
+    max = maxToken > 8192 ? 8192 : maxToken;
   }
 
   /* 4.0 preview */
-  if(model.includes('preview')){
-    max = maxToken > 4096 ? 4096 : maxToken
+  if (model.includes('preview')) {
+    max = maxToken > 4096 ? 4096 : maxToken;
   }
 
   /* 4.0 32k */
-  if(model.includes('32k')){
-    max = maxToken > 32768 ? 32768 : maxToken
+  if (model.includes('32k')) {
+    max = maxToken > 32768 ? 32768 : maxToken;
   }
- 
-  return max
+
+  return max;
 }
