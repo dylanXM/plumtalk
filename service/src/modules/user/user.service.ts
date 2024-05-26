58f44b0e8e442aafabe3a53cc31794a9ee0d3614
dylan_xm <dylan_xm@163.com>
2024-05-26 22:03:40 +0800

    feat: 增加IDfanhui

--- service/src/modules/user/user.service.ts
+++ service/src/modules/user/user.service.ts
@@ -235,7 +235,7 @@ export class UserService {
   async getUserInfo(userId: number) {
     const userInfo: any = await this.userEntity.findOne({
       where: { id: userId },
-      select: ['username', 'avatar', 'role', 'email', 'sign', 'inviteCode', 'openId', 'consecutiveDays'],
+      select: ['username', 'avatar', 'role', 'email', 'sign', 'inviteCode', 'openId', 'consecutiveDays', 'id'],
     });
     if (!userInfo) {
       throw new HttpException('当前用户信息失效、请重新登录！', HttpStatus.UNAUTHORIZED);
